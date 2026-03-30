af4a3b0b3cfb793f375cf8f5825dc61f72cac03d
Sura Saber <YOUR_GITHUB_EMAIL@example.com>
2026-03-30 22:28:26 +0300

    Remove files not needed for GitHub Pages

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "blog"
-
-urlpatterns = [
-    path("", views.blogPage, name="blogPage"),
-    path("<slug:slug>/", views.blogDetail, name="blogDetail"),
-]
--- courses/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.homePage,name='homePage'),
-    path('courses/', views.course_list, name='course_list'),
-    path('courses/<slug:slug>/', views.course_detail, name='course_detail'),
-    path('video/<slug:slug>/', views.course_video, name='course_video'),
-]
